3f9c2afde41fc76dd7553697d324f9e440769cf0
danielemantovani <daniele.mantovani124@gmail.com>
2024-04-20 15:51:12 +0200

    feat: bonus

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ const { createApp } = Vue;
 createApp ({
     data (){
         return {
+            newItem: {
+                text: "",
+                done: true,
+            },
             todoList: [
                 {
                     text: "Recruiting blog post",
@@ -24,6 +28,30 @@ createApp ({
         }
     },
     methods: {
+        addItem: function (){
+            console.log(this.newItem);
+            if (this.newItem.text !== ""){
+                const copyTodo = {...this.newItem}
+                this.todoList.push(copyTodo);
+                this.newItem.text = "";
+            }
+        },
         
+        doneItem: function(index){
+            console.log("ciao");
+            //se il todo di index è true 
+            //diventa false
+            //altrimenti è true
+            if (this.todoList[index].done === true){
+                this.todoList[index].done = false;
+            } else {
+                this.todoList[index].done = true;
+            }
+        },
+
+        cancel: function(index){
+            //splice elimina dall'elemento passato 
+            this.todoList.splice(index,1)
+        }
     }
 }).mount ("#app")
