5981dd9ec28ca354c70a2dbb294abc8a12e8eb27
danielemantovani <daniele.mantovani124@gmail.com>
2024-04-20 12:01:57 +0200

    feat: item list

--- js/script.js
+++ js/script.js
@@ -2,8 +2,25 @@ const { createApp } = Vue;
 
 createApp ({
     data (){
-        return{
-            greeting: "",
+        return {
+            todoList: [
+                {
+                    text: "Recruiting blog post",
+                    done: true,
+                },
+                {
+                    text: "Mobile app luaunch",
+                    done: true,
+                },
+                {
+                    text: "Interview Jhon H.",
+                    done: false,
+                },
+                {
+                    text: "Summit update to mobile storefronts",
+                    done: true,
+                }
+            ]
         }
     },
     methods: {
